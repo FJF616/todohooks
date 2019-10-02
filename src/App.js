f575a88b8869922c483c82da1f439203d7d8fcfa
Franz Fernando <ffernando@engineer.com>
2019-10-01 21:45:44 -0700

    adds todo functions

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [todoList, setTodoList] = useState([
+    {
+      todo: "example task",
+      completed: false
+    }
+  ]);
+
+  //adds a todo to the todo list
+  const addTodo = todo => {
+    const newTodo = [...todoList, { todo }];
+    setTodoList(newTodo);
+  };
+
+  //indicates if todo is complete
+  const completeTodo = index => {
+    const updatedTodoList = [...todoList];
+    updatedTodoList[index].completed = true;
+    setTodoList(updatedTodoList);
+  };
+
+  //removes a todo from the list
+  const removeTodo = index => {
+    const updatedTodoList = todoList.splice();
+    updatedTodoList.splice(index, 1);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
