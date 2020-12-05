2ddf83e4da2049232a22d53331343bf94d71d8a6
Franz Fernando <ffernando@engineer.com>
2020-12-05 13:36:33 -0800

    Create useEffectOnce.js

---
+++ src/components/hooks/useEffectOnce.js
@@ -0,0 +1,12 @@
+import { useRef, useEffect } from "react";
+
+export default function useEffectOnce(cb) {
+  const didRun = useRef(false);
+
+  useEffect(() => {
+    if (!didRun.current) {
+      cb();
+      didRun.current = true;
+    }
+  });
+}
